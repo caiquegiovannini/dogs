ef8911bbc05a180b89c3095f6bf68040e7767a24
Caique <caique_giovannini@hotmail.com>
2020-10-16 21:43:27 -0300

    add error treatment at create user

--- src/components/Login/LoginForm/index.js
+++ src/components/Login/LoginForm/index.js
@@ -5,6 +5,10 @@ import { UserContext } from '../../../contexts/UserContext';
 
 import Button from '../../Form/Button';
 import Input from '../../Form/Input';
+import Error from '../../Hepler/Error';
+
+import './styles.css';
+import '../../Form/Button/styles.css';
 
 const LoginForm = () => {
   const username = useForm();
@@ -21,9 +25,9 @@ const LoginForm = () => {
   }
 
   return (
-    <section>
-      <h1>Login</h1>
-      <form onSubmit={handleSubmit}>
+    <section className="animeLeft">
+      <h1 className="title">Login</h1>
+      <form className="form" onSubmit={handleSubmit}>
         <Input label="Usuário" type="text" name="username" {...username} />
         <Input label="Senha" type="password" name="password" {...password} />
 
@@ -32,10 +36,15 @@ const LoginForm = () => {
           : <Button>Entrar</Button>
         }
 
-        {error && <p>{error}</p>}
+        <Error error={error} />
       </form>
 
-      <Link to="/login/criar">Cadastro</Link>
+      <Link className="perdeu" to="/login/perdeu">Perdeu a senha?</Link>
+      <div className="cadastro">
+        <h2 className="subtitle">Cadastre-se</h2>
+        <p>Ainda não possui uma conta? Cadastre-se no site.</p>
+        <Link to="/login/criar" className="button">Cadastro</Link>
+      </div>
     </section>
   );
 };
